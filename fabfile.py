c94f47ceffd97482808dd86417d05f27d13d8261
James Fogarty <jayfo.github@gmail.com>
2014-05-28 06:23:43 -0700

    Fabric updates

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,10 @@ fabric.api.env.hosts = ['barb.cs.washington.edu']
 fabric.api.env.user = 'jfogarty'
 
 
+def build():
+    fabric.api.local('jekyll build --config _config.yml')
+
+
 def deploy():
     # Locally build the website
     fabric.api.local('jekyll build --config _config.yml')
@@ -24,3 +28,9 @@ def deploy():
 
 def serve():
     fabric.api.local('jekyll serve --config _config.yml,_config-dev.yml --watch --force_polling')
+
+
+# We cannot have a test because Fabric requires Python 2.7 but our tests use Python 3.4
+#
+# def test():
+#    fabric.api.local('nosetests')
